3d9f2f808d444e2d53cc5c50d02eb1bd2e4bf7f3
Peace <peacexd55@gmail.com>
2026-05-13 19:42:41 +0530

    v2

--- nqueen.cpp
+++ nqueen.cpp
@@ -1,38 +1,69 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
 using namespace std;
 
-int x[20], count = 1;
+int board[10]; // board[row] = column position of queen
 
-bool place(int r, int c) {
-    for (int i = 1; i < r; i++)
-        if (x[i] == c || abs(x[i] - c) == abs(i - r))
+// Function to check whether queen can be placed safely
+bool isSafe(int row, int col)
+{
+    for(int i = 1; i < row; i++)
+    {
+        // Check same column
+        if(board[i] == col)
             return false;
+
+        // Check diagonal
+        if(abs(board[i] - col) == abs(i - row))
+            return false;
+    }
+
     return true;
 }
 
-void NQueen(int r, int n) {
-    if (r > n) {
-        cout << "\n\nSOLUTION: " << count++ << "\n\n";
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++)
-                cout << (x[i] == j ? " Q " : " - ");
+// Function to place queens
+void solve(int row, int n)
+{
+    // If all queens are placed
+    if(row > n)
+    {
+        cout << "\nSolution:\n";
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(board[i] == j)
+                    cout << " Q ";
+                else
+                    cout << " - ";
+            }
             cout << endl;
         }
-    } else {
-        for (int c = 1; c <= n; c++) {
-            if (place(r, c)) {
-                x[r] = c;
-                NQueen(r + 1, n);
-            }
+
+        return;
+    }
+
+    // Try placing queen in every column
+    for(int col = 1; col <= n; col++)
+    {
+        // Check if position is safe
+        if(isSafe(row, col))
+        {
+            board[row] = col;   // Place queen
+
+            solve(row + 1, n);  // Move to next row
         }
     }
 }
 
-int main() {
+int main()
+{
     int n;
-    cout << "ENTER THE NUMBER OF QUEEN'S: ";
+
+    cout << "Enter number of queens: ";
     cin >> n;
-    NQueen(1, n);
+
+    solve(1, n);
+
     return 0;
 }
